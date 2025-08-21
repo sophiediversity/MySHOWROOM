a749db4f9a206b2accb0daf8afd99ab9a51f1522
BackToPlan <150146109+sophiediversity@users.noreply.github.com>
2025-08-21 18:29:45 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -133,31 +133,26 @@
         // Add an event listener listening for scroll
         window.addEventListener('scroll', navHighlight);
 
-        function navHighlight() {
-        
-            // Get current scroll position
-            let scrollY = window.pageYOffset;
-        
-            // Loop through sections to get height(including padding and border), 
-            // top and ID values for each
-            sections.forEach(function(current) {
-                const sectionHeight = current.offsetHeight;
-                const sectionTop = current.offsetTop - 50;
-                const sectionId = current.getAttribute('id');
-            
-               /* If our current scroll position enters the space where current section 
-                * on screen is, add .current class to parent element(li) of the thecorresponding 
-                * navigation link, else remove it. To know which link is active, we use 
-                * sectionId variable we are getting while looping through sections as 
-                * an selector
-                */
-                if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
-                    document.querySelector('.header-nav a[href*=' + sectionId + ']').parentNode.classList.add('current');
-                } else {
-                    document.querySelector('.header-nav a[href*=' + sectionId + ']').parentNode.classList.remove('current');
-                }
-            });
+     function navHighlight() {
+
+    let scrollY = window.pageYOffset;
+
+    sections.forEach(function(current) {
+        const sectionHeight = current.offsetHeight;
+        const sectionTop = current.offsetTop - 50;
+        const sectionId = current.getAttribute('id');
+
+        const link = document.querySelector('.header-nav a[href*="' + sectionId + '"]');
+        if (!link || !link.parentNode) return; // <-- Absicherung
+
+        if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+            link.parentNode.classList.add('current');
+        } else {
+            link.parentNode.classList.remove('current');
         }
+    });
+}
+
 
     }; // end ssScrollSpy
 
